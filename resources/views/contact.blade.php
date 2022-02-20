1b772c5750ac3bb0d5d913c19521f5374479cfcc
Kevin Wijaya <wijayakevin2003@gmail.com>
2022-02-20 23:08:40 +0700

    mid project lnt backend

--- resources/views/contact.blade.php
+++ resources/views/contact.blade.php
@@ -6,10 +6,10 @@
     <div class="container mt-4">
         <h2 class="header"><i class="uil uil-user-square me-2"></i>Contact Us</h2>
         <hr>
-        <div class="col-md-6 bg-light manage-wrapper">
-            <h4> <i class="uil uil-envelope me-2"></i>Email: musanglibrary@gmail.com</h4>
-            <h4> <i class="uil uil-phone me-2"></i>Phone: +62 722 707 707</h4>
-            <h4> <i class="uil uil-map-marker me-2"></i>Address: P.O Box 92480, Jakarta, Indonesia</h4>
+        <div class="col-md-5 bg-light manage-wrapper">
+            <h5> <i class="uil uil-envelope me-2"></i>Email: musanglibrary@gmail.com</h5>
+            <h5> <i class="uil uil-phone me-2"></i>Phone: +62 722 707 707</h5>
+            <h5> <i class="uil uil-map-marker me-2"></i>Address: P.O Box 92480, Jakarta, Indonesia</h5>
         </div>
     </div>
 @endsection
